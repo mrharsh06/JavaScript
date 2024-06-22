f1012c2a83fcd1ef7f2b9b80d834256d79367a6c
Harsh Chaudhary <56648371+mrharsh06@users.noreply.github.com>
2024-06-22 11:44:23 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=123456
+let accountEmail="mr.harsh0606@gmail.com"
+var accountPassword='55555'
+accountCity="bulandshahr" //try to avoid this practice
+let accountState
+// accountId=2  //it is not possible to change const value
+accountEmail="chaudhary@gmail.com"  //but we can chnage the value of let keywords
+accountPassword='11111'
+accountCity="bengaluru"
+console.log([accountId,accountEmail,accountPassword,accountCity,accountState])
+//never use var variable because it has the issue with block and functional scope use let keyword instead of this
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert("hello")
+//we are using node js, not browser so alert will throw error in this if we are using in browser then it will run
+let name="Harsh"
+let age=18
+let isLoggedIn=false
+
+//primitive datatypes
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null =>standalone value
+//undefind=>
+//symbol=>unique
+
+//object
+
+console.log(typeof null); //object
+
+console.log(typeof undefined); //undefined
